f00a9e2cc0157b001406aadb0a6335011303250a
Nepokornyi <vadymkin@gmail.com>
2024-07-05 14:04:47 +0200

    chore: add tailwind config

--- portfolio-next/src/components/Button/Button.tsx
+++ portfolio-next/src/components/Button/Button.tsx
@@ -8,14 +8,16 @@ type ButtonProps = {
 
 export const Button = ({
     children,
-    color = '#ffd600',
+    color = 'primary-button',
     className = '',
 }: ButtonProps) => {
     return (
         <button
-            className={`px-4 py-2 lg:px-6 lg:py-3 bg-yellow-500 text-black font-bold text-base lg:text-xl italic group ${className} `}
+            className={`px-4 py-2 lg:px-6 lg:py-3 bg-${color} text-black font-bold text-base lg:text-xl italic group ${className} `}
         >
-            <span className="absolute border border-yellow-500 inset-2 w-full h-full -z-10 duration-300 group-hover:inset-1" />
+            <span
+                className={`absolute border border-${color} inset-2 w-full h-full -z-10 duration-300 group-hover:inset-1`}
+            />
             {children}
         </button>
     )
--- portfolio-next/tailwind.config.js
+++ portfolio-next/tailwind.config.js
@@ -17,7 +17,29 @@ module.exports = {
     content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
     darkMode: false,
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    highlight: '#FFC700',
+                    button: '#FFD600',
+                    'light-green': '#40C8A5',
+                    green: '#40C8C8',
+                    'dark-blue': '#039BB8',
+                    blue: '#29A1F8',
+                    stroke: '#A6EAFF',
+                    background: '#000000',
+                    text: '#FFFFFF',
+                },
+                secondary: {
+                    discord: '#9E52FF',
+                    github: '#B540C8',
+                    linkedin: '#1867FF',
+                    yellow: '#FFD600',
+                    green: '#ADFF00',
+                    red: '#FF3D29',
+                },
+            },
+        },
     },
     plugins: [rotateX],
 }
