2fb97c45c844ce0143e1f37632cfa19cec0decab
Nepokornyi <vadymkin@gmail.com>
2024-07-07 19:44:01 +0200

    feat: glitched logo gradient & pause on hover

--- portfolio-next/src/components/Header/components/GlitchedLogo/GlitchedLogo.tsx
+++ portfolio-next/src/components/Header/components/GlitchedLogo/GlitchedLogo.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Image from 'next/image'
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useGlitch } from 'react-powerglitch'
 import { glitchConfig } from './glitchConfig'
 
@@ -9,12 +9,28 @@ import logo from '../../../../assets/static/icoLogo.svg'
 export const GlitchedLogo = () => {
     const glitch = useGlitch(glitchConfig)
 
+    const handleMouseEnter = useCallback(() => {
+        glitch.stopGlitch()
+    }, [glitch])
+
+    const handleMouseLeave = useCallback(() => {
+        glitch.startGlitch()
+    }, [glitch])
+
+    // gradient has same props as design
     return (
-        <Image
-            ref={glitch.ref}
-            src={logo}
-            alt="logo"
-            className="object-fill w-[32px] lg:w-[64px]"
-        />
+        <div className="relative p-4">
+            <div
+                className={`absolute top-0 pointer-events-none left-0 w-full h-full rounded-full bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#0775bd] to-black blur-[32px] lg:blur-2xl z-10`}
+            />
+            <Image
+                ref={glitch.ref}
+                src={logo}
+                alt="logo"
+                className={`object-fill w-[32px] lg:w-[64px] hover:${glitch.stopGlitch}`}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+            />
+        </div>
     )
 }
