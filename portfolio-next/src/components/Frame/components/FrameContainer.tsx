65b2924bb303479c2e49c6b1e55fa4282f376a04
Nepokornyi <vadymkin@gmail.com>
2024-07-21 12:08:45 +0200

    feat: add max-width to FrameContainer

--- portfolio-next/src/components/Frame/components/FrameContainer.tsx
+++ portfolio-next/src/components/Frame/components/FrameContainer.tsx
@@ -3,20 +3,25 @@ import React, { ReactNode } from 'react'
 type FrameContainerProps = {
     children: ReactNode
     className?: string
+    isHalfWidth?: boolean
 }
 
 export const FrameContainer = ({
     children,
     className,
+    isHalfWidth = false,
 }: FrameContainerProps) => {
     return (
         <div
-            className={`rectangle w-auto grid items-center relative ${className}`}
+            className={`rectangle w-full ${
+                isHalfWidth ? 'max-w-[960px]' : 'max-w-[1920px]'
+            } grid items-center relative ${className}`}
         >
             <div className="line top-left" />
             <div className="line top-right" />
             <div className="line bottom-left" />
             <div className="top-horizontal" />
+            <div className="left-vertical" />
             {children}
         </div>
     )
